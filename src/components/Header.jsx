52d3dad7996345b721f95ada626a6496e3aba352
GhaliMahdi <ghali.mahdi99@gmail.com>
2023-01-12 20:33:07 +0100

    Added the useIsLoggedIn custom hook to the header to check if user is logged in or not and alternate between showing login/logout and showing or not showing cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import { ShoppingCartContext, UserContext } from '../App';
+import { useIsLoggedIn } from '../hooks/IsLoggedInHook';
 
 export const Header = () => {
     const [cart] = useContext(ShoppingCartContext);
     const [user] = useContext(UserContext);
+    const isLoggedIn = useIsLoggedIn();
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Link to="/"><Navbar.Brand>Shopping Cart {user.user?.username}</Navbar.Brand></Link>
-        {user.token && <Link to="/cart">Cart {cart.length}</Link>}
-        {user.token && <Link to="/logout">Logout</Link>}
-        {!user.token && <Link to="/login">Login</Link>}
+        {isLoggedIn && <Link to="/cart">Cart {cart.length}</Link>}
+        {isLoggedIn && <Link to="/logout">Logout</Link>}
+        {!isLoggedIn && <Link to="/login">Login</Link>}
       </Container>
     </Navbar>
   )
